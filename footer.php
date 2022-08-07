50eaff218667bbb4fa4e6fbac063deea1c4a1a07
info-Nytta <info@nytta.hu>
2022-08-07 21:35:53 +0200

    láb

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 	</main>
 	<footer>
-		design and programming<br>by Nytta
+	<p style='font-style:italic;'>
+		design & programming by <a href='https://nytta.hu' target='_blank'>Nytta</a> <?php echo date("Y")."."; ?>
+	</p>
 	</footer>
 </div>
 </body>
