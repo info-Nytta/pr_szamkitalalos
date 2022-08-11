d8564d9e93f56fc3aa9778f3790ae912a4178b61
info-Nytta <58329400+info-Nytta@users.noreply.github.com>
2022-08-11 18:48:39 +0200

    jelki beillesztés

--- p_jatek.php
+++ p_jatek.php
@@ -36,17 +36,8 @@ else
 
     <h2><?php echo $gondol?> gondol</h2>
     
-    <p class='jelek center'>
-			<?php 
-			for ($i=1; $i<$min; $i++) echo "<span class='tilt jel'>x</span>";
-			echo "<span class='enged szam'> ".$min." </span>";
-			for ($i=$min+1; $i<$max; $i++) echo "<span class='enged jel'>?</span>";
-			echo "<span class='jel-kicsi'> - </span>";
-			if ($min!=$max) echo "<span class='enged szam'> ".$max." </span>";
-			for ($i=100; $i>$max; $i--) echo "<span class='tilt jel'>x</span>";
-			?>
-	</p>
-    
+<? include('jelki.php'); ?>
+
     <h3><?php echo $tippel?> tippjei</h3>
     <p>
     <?php echo $tippek; ?>
